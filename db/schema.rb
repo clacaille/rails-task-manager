c5a7f81e6fc717a8aa0249cc0cc35d4c9fdfafb4
Cesar Lacaille <cesar.lacaille@hotmail.fr>
2017-02-07 11:31:43 +0100

    model task created

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170207102359) do
+ActiveRecord::Schema.define(version: 20170207103056) do
 
   create_table "tasks", force: :cascade do |t|
+    t.string   "name"
+    t.boolean  "done"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
