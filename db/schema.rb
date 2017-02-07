89e94511209a53443f1a18e0b805a7cc2dd1d2fc
Cesar Lacaille <cesar.lacaille@hotmail.fr>
2017-02-07 11:39:24 +0100

    remove column done from tasks

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170207103056) do
+ActiveRecord::Schema.define(version: 20170207103821) do
 
   create_table "tasks", force: :cascade do |t|
     t.string   "name"
-    t.boolean  "done"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
